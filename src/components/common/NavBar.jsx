f0f097a82c746b41c8715ceda75495118e6a9c85
Dave Beach <hello@davebeach.me>
2025-08-01 22:31:45 -0400

    feat(navbar): :sparkles: add navbar component

---
+++ src/components/common/NavBar.jsx
@@ -0,0 +1,71 @@
+import gsap from 'gsap';
+import { useEffect, useRef, useState } from 'react';
+import pkg from 'react-use';
+
+const { useWindowScroll } = pkg;
+
+const navItems = ['About', 'Services', 'Projects', 'Contact'];
+
+const NavBar = () => {
+  const [isIndicatorActive, setIsIndicatorActive] = useState(false);
+
+  const navContainerRef = useRef(null);
+
+  const { y: currentScrollY } = useWindowScroll();
+  const [isNavVisible, setIsNavVisible] = useState(true);
+  const [prevScrollY, setPrevScrollY] = useState(0);
+
+  useEffect(() => {
+    if (currentScrollY === 0) {
+      setIsNavVisible(true);
+      navContainerRef.current.classList.remove('floating-nav');
+    } else if (currentScrollY > prevScrollY) {
+      setIsNavVisible(false);
+      navContainerRef.current.classList.add('floating-nav');
+    } else if (currentScrollY < prevScrollY) {
+      setIsNavVisible(true);
+      navContainerRef.current.classList.add('floating-nav');
+    }
+
+    setLastScrollY(currentScrollY);
+  }, [currentScrollY, prevScrollY]);
+
+  useEffect(() => {
+    gsap.to(navContainerRef.current, {
+      y: isNavVisible ? 0 : -100,
+      opacity: isNavVisible ? 1 : 0,
+      duration: 0.2,
+    });
+  }, [isNavVisible]);
+
+  return (
+    <div
+      className="fixed inset-x-0 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6"
+      ref={navContainerRef}
+    >
+      <header className="absolute top-1/2 w-full -translate-y-1/2">
+        <nav className="flex size-full items-center justify-between p-4">
+          <div className="flex items-center gap-7">
+            <img className="w-10" src="" alt="Northern Venture logo" />
+          </div>
+
+          <div className="flex h-full items-center">
+            <div className="hidden md:block">
+              {navItems.map((item, index) => (
+                <a
+                  className="nav-hover-btn"
+                  key={index}
+                  href={`#${item.toLowerCase()}`}
+                >
+                  {item}
+                </a>
+              ))}
+            </div>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default NavBar;
