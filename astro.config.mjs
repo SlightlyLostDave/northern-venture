ec58c77b92ff4a745e2ce96ed3b06d561d152ae6
Dave Beach <hello@davebeach.me>
2026-02-09 20:25:03 -0500

    feat(layout): :chart_with_upwards_trend: add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
-
 import icon from 'astro-icon';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,6 +16,11 @@ export default defineConfig({
     icon({
       include: {
         mdi: ['linkedin', 'instagram', 'email-outline'],
+      },
+    }),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
       },
     }),
   ],
